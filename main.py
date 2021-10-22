81657bc29c3fff3cfee8de9801d4ce5901151be3
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-10-21 22:21:09 -0300

    Choose the coin and print coin value when it is changed

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import urllib.request
+from threading import Timer
 
-page = urllib.request.urlopen('https://coinmarketcap.com/currencies/electric-vehicle-direct-currency/')
-page = str(page.read())
+coin = input('Digite o nome completo da moeda que deseja acompanhar: ')
+coin = (coin.replace(" ", "-")).lower()
+min = input('Digite o valor para comparação: ')
 
-firstLocation = page.find('<div class="sc-16r8icm-0 kjciSH priceTitle">')
-finalLocation = page.index('</div>', firstLocation)
-priceLocation = page.index('$', firstLocation)
+print('Loading...')
 
-print(page[priceLocation:finalLocation])
+pastPrice = ''
+
+def getCoinValue():
+    page = urllib.request.urlopen('https://coinmarketcap.com/currencies/' + coin + '/')
+    page = str(page.read())
+    global pastPrice
+
+    firstLocation = page.find('<div class="sc-16r8icm-0 kjciSH priceTitle">')
+    finalLocation = page.index('</div>', firstLocation)
+    priceLocation = page.index('$', firstLocation)
+
+    if page[priceLocation:finalLocation] != pastPrice:
+        print('--------------------')
+        print((coin.replace("-", " ")).capitalize())
+        print('Valor atual: ' + page[priceLocation:finalLocation])
+
+    Timer(10.0, getCoinValue).start()
+    pastPrice = page[priceLocation:finalLocation]
+
+
+Timer(1, getCoinValue).start()
 
